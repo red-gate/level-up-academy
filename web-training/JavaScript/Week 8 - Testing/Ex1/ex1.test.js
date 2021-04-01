cd4e3094a0d11eb9e4a190acc8d62687b6e78da2
Matthew Chappelow <matthew.chappelow@red-gate.com>
2021-04-01 10:39:51 +0100

    Update promping string with better hint.

--- web-training/JavaScript/Week 8 - Testing/Ex1/ex1.test.js
+++ web-training/JavaScript/Week 8 - Testing/Ex1/ex1.test.js
@@ -1,13 +1,14 @@
 const fraction = require ('./ex1.js');
 
 describe('Fraction', function () {
+
   test('toNumber() works for 1/2', () => {
     const a = new fraction.Fraction(1, 2);
     expect(a.toNumber()).toBe(0.5);  
   });
 
   // Add a single toNumber() test for 1/3
-  // Add a single toString() test
+  // Add a single toString() test that asserts the ToString of a fraction is in fractional form
   // Test that that constructed fraction has correct properties ({ numerator, denominator })
-  // Test that the fraction constructors throws an exception when a non-numeric parameter is passed to it    
+  // Test that the fraction constructors throws an exception when a non-numeric parameter is passed to it
 });
