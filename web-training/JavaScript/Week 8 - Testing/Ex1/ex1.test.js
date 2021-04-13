cb6bc4da52e5fffd706b9f018bb0e36b0d74a9fd
Sam Blackburn <sam.blackburn@red-gate.com>
2021-04-13 10:55:46 +0100

    Complete Ex1

--- web-training/JavaScript/Week 8 - Testing/Ex1/ex1.test.js
+++ web-training/JavaScript/Week 8 - Testing/Ex1/ex1.test.js
@@ -8,7 +8,30 @@ describe('Fraction', function () {
   });
 
   // Add a single toNumber() test for 1/3
+  test('toNumber() works for 1/3', () => {
+    const a = new fraction.Fraction(1, 3);
+    expect(a.toNumber()).toBeCloseTo(0.33333333, 8);
+  });
+  
   // Add a single toString() test that asserts the ToString of a fraction is in fractional form
+  test('toString() works for 1/2', () => {
+    const a = new fraction.Fraction(1, 2);
+    expect(a.toString()).toBe("1/2");
+  });
+  
   // Test that that constructed fraction has correct properties ({ numerator, denominator })
+  test('numerator and denominator are as expected', () => {
+    const a = new fraction.Fraction(1, 2);
+    expect(a).toMatchObject({numerator:1, denominator:2});
+  });
+  
   // Test that the fraction constructors throws an exception when a non-numeric parameter is passed to it
+  describe ('when passed invalid input', () => {
+      test('throws when numerator is not a number', () => {
+        expect(() => new fraction.Fraction('invalid', 2)).toThrow("Expected the numerator to be an integer number");
+      });
+      test('throws when denominator is not a number', () => {
+        expect(() => new fraction.Fraction(1, 'invalid')).toThrow("Expected the denominator to be an integer number");
+      });
+  });
 });
