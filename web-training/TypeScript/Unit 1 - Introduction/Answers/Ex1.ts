1507379d89b8c123750cbcdf0e1a5ec3c413f08f
Piers Williams <piers.williams@red-gate.com>
2021-04-21 09:11:38 +0100

    Add answer for part c ex1 typescript unit 1

--- web-training/TypeScript/Unit 1 - Introduction/Answers/Ex1.ts
+++ web-training/TypeScript/Unit 1 - Introduction/Answers/Ex1.ts
@@ -12,3 +12,21 @@ function Fibonacci(n: number): number {
 }
 
 console.log(Fibonacci(7));
+
+function Fibonnaci(index: number, previousValues?: number[]): number {
+    if(index === 0) return 0;
+    if(index === 1) return 1;
+    if(!previousValues){
+        previousValues = Array(index + 1).fill(-1);
+    }
+    console.log(previousValues);
+    if(previousValues[index] !== -1){
+        console.log("Found old value" + previousValues[index]);
+        return previousValues[index];
+    }
+    var num = Fibonnaci(index - 1, previousValues) + Fibonnaci(index - 2, previousValues);
+    previousValues[index] = num;
+    return num;
+}
+
+console.log(Fibonnaci(8));
