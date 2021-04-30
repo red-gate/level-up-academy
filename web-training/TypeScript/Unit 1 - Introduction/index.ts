a2651ba23a076b98ec775112ffe1530886afa17f
Sam Blackburn <sam.blackburn@red-gate.com>
2021-04-30 14:35:33 +0100

    Import MathApi, implement and use it

--- web-training/TypeScript/Unit 1 - Introduction/index.ts
+++ web-training/TypeScript/Unit 1 - Introduction/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world");
+import {IMathApi, Fraction} from "./Examples/fraction"
+class MathApi implements IMathApi {
+    divide(num: number, den: number): number {
+        return num / den;
+    }
+}
+
+let f = new Fraction(3, 4, { divide: (num, den) => {
+    return num/den;
+}});
+console.log(f.toNumber(console.log));
