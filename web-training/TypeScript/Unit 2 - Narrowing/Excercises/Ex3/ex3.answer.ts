c34a1fe7469c90bf543e1e5b4b2c759676c5b331
Alex Kondratov <alex.kondratov@red-gate.com>
2021-05-06 11:36:09 +0100

    Add Array.find() excercise

---
+++ web-training/TypeScript/Unit 2 - Narrowing/Excercises/Ex3/ex3.answer.ts
@@ -0,0 +1,15 @@
+// const list = ["Pierce", "Sami", "Mark", "Alex"];
+
+// function FindByFirstLetter(letter: string) {
+//     return list.find(name => name[0] == letter);
+// }
+
+// function Greet(name: string) {
+//     console.log(`Hello ${name}!`);
+// }
+
+// const nameToGreet = FindByFirstLetter("P");
+
+// if (nameToGreet) {
+//     Greet(nameToGreet);
+// }
---
+++ web-training/TypeScript/Unit 2 - Narrowing/Excercises/Ex3/ex3.ts
@@ -0,0 +1,11 @@
+const list = ["Pierce", "Sami", "Mark", "Alex"];
+
+function FindByFirstLetter(letter: string) {
+    return list.find(name => name[0] == letter);
+}
+
+function Greet(name: string) {
+    console.log(`Hello ${name}!`);
+}
+
+Greet(FindByFirstLetter("P"));
