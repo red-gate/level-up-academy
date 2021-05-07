ed620d829025ee9703ed24027e46c422dda0d29b
Piers Williams <piers.williams@red-gate.com>
2021-05-07 08:32:03 +0100

    Correct spelling of Exercises

--- web-training/TypeScript/Unit 2 - Narrowing/Excercises/ex2.ts
+++
@@ -1,7 +0,0 @@
-// TODO: replace 'any' with the correct type
-function welcomePeople(people: any) {
-  // TODO: write the implementation
-}
-
-welcomePeople("Gandalf");
-welcomePeople(["Chip", "Dale", "the Smurfs"]);
---
+++ web-training/TypeScript/Unit 2 - Narrowing/Exercises/ex1.ts
@@ -0,0 +1,6 @@
+function printId(id: number | string) {
+  console.log("Your ID is: " + id.toUpperCase());
+}
+
+printId(123);
+printId("456");
