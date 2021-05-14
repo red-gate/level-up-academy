517b872e43e63e089ea9f5241722d39968361bec
Sami Lamti <sami.lamti@red-gate.com>
2021-05-14 13:48:32 +0100

    Add answer

---
+++ web-training/TypeScript/Unit 2 - Narrowing/Exercises/Ex2/ex2.answer.ts
@@ -0,0 +1,11 @@
+// function welcomePeople(people: string | string[]) {
+//   if (Array.isArray(people)) {
+//     console.log(`Hello ${people.slice(0, people.length -1).join(', ')} and ${people[people.length -1]}`);
+//   }
+//   else {
+//     console.log(`Welcome lone traveller ${people}`);
+//   }
+// }
+
+// welcomePeople("Gandalf");
+// welcomePeople(["Chip", "Dale", "the Smurfs"]);
