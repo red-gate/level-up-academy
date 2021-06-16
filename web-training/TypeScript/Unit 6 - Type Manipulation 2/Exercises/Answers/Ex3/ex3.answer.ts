d615c387e860aa7b6b718a27f2931af66ce4d9ff
Mark Jordan <mark.jordan@red-gate.com>
2021-06-16 14:11:46 +0100

    clean up exercise scripts in package.json
    don't run the exercise unless there's actually some executable code to
    run

--- web-training/TypeScript/Unit 6 - Type Manipulation 2/Exercises/Answers/Ex3/ex3.answer.ts
+++ web-training/TypeScript/Unit 6 - Type Manipulation 2/Exercises/Answers/Ex3/ex3.answer.ts
@@ -1,22 +1,20 @@
 class Animal {
-    constructor(public name: string){}
+  constructor(public name: string) {}
 }
 
 class Cat extends Animal {
-    constructor(public name: string, public numberOfLives: number){
-        super(name);
-    }
+  constructor(public name: string, public numberOfLives: number) {
+    super(name);
+  }
 }
 
 class Dog extends Animal {
-    constructor(public name: string, public goodBoy: boolean){
-        super(name);
-    }
+  constructor(public name: string, public goodBoy: boolean) {
+    super(name);
+  }
 }
 
-class Platypus {
-
-}
+class Platypus {}
 
 const beast = new Cat("Meow", 9); // Must be its name, what it said when I asked it nicely
 
@@ -31,40 +29,42 @@ In our case, it initializes the tag property.
 type AnimalConstructor<T = Animal> = new (...args: any[]) => T;
 
 function Own<TBase extends AnimalConstructor>(Base: TBase) {
-    return class Owned extends Base {
-        // Mixins may not declare private/protected properties
-		//	so ES2020 private fields are the best we have
-        _ownerName = "";
+  return class Owned extends Base {
+    // Mixins may not declare private/protected properties
+    //	so ES2020 private fields are the best we have
+    _ownerName = "";
 
-        setOwner(owner: string){
-            this._ownerName = owner;
-        }
-
-        get owner(): string {
-            return this._ownerName;
-        }
+    setOwner(owner: string) {
+      console.log(`${this.name} is now owned by ${owner}`);
+      this._ownerName = owner;
     }
+
+    get owner(): string {
+      return this._ownerName;
+    }
+  };
 }
 
 function Trainable<TBase extends AnimalConstructor>(Base: TBase) {
-    return class Traned extends Base {
-        _trained = false;
-        train() {
-            this._trained = true;
-        }
-        get trained(): boolean {
-            return this._trained;
-        }
+  return class Traned extends Base {
+    _trained = false;
+    train() {
+      console.log(`${this.name} was trained`);
+      this._trained = true;
     }
+    get trained(): boolean {
+      return this._trained;
+    }
+  };
 }
 
 const OwnedCat = Own(Cat);
 const OwnedDog = Own(Dog);
+// @ts-expect-error
 const OwnedPlatypus = Own(Platypus); // Can't own a platypus!
 
 const felix = new OwnedCat("Felix", 9);
 felix.setOwner("Piers");
-
 
 const TrainedCat = Trainable(Cat);
 const TrainedOwnedCat = Trainable(Own(Cat));
