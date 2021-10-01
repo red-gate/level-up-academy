5e399776bd2bf4088c08a835ecd3d8a1858cd100
The Milk Man <ajayseanx@gmail.com>
2021-10-01 14:38:48 +0100

    add UtcDateTime

---
+++ tactical-refactoring/stringly-typing/AccountCalculator/UtcDateTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AccountCalculator
+{
+    public class UtcDateTime
+    {
+        private DateTime _dateTime;
+
+        public UtcDateTime(DateTimeOffset dateTimeOffset) => _dateTime = dateTimeOffset.UtcDateTime;
+
+        public DateTime Get() => _dateTime;
+    }
+}
