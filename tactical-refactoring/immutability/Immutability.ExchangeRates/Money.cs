a1b8037536682d056462dea0344f043085ba9a20
Ben Wood <ben.wood@red-gate.com>
2021-10-15 14:30:38 +0100

    Make money a record

--- tactical-refactoring/immutability/Immutability.ExchangeRates/Money.cs
+++ tactical-refactoring/immutability/Immutability.ExchangeRates/Money.cs
@@ -1,43 +1,7 @@
-using System;
-
 namespace Immutability.ExchangeRates
 {
-    public sealed class Money
+    public sealed record Money(Currency Currency, decimal Amount)
     {
-        public Currency Currency { get; }
-        public decimal Amount { get; }
-
-        public Money(Currency currency, decimal amount)
-        {
-            Currency = currency;
-            Amount = amount;
-        }
-
-        private bool Equals(Money other)
-        {
-            return Currency.Equals(other.Currency) && Amount == other.Amount;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return ReferenceEquals(this, obj) || obj is Money other && Equals(other);
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Currency, Amount);
-        }
-
-        public static bool operator ==(Money left, Money right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(Money left, Money right)
-        {
-            return !Equals(left, right);
-        }
-
         public override string ToString()
         {
             return Currency.Code + '\u00A0' + Amount;
