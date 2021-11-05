c29687780d723f7ed9377ece8957e70188c800f5
Sam Blackburn <sam.blackburn@red-gate.com>
2021-11-05 15:40:40 +0000

    Fix up DI

--- tactical-refactoring/extracting-classes/BlueBridge.SeaQuollMonitor.Management/ManagementModule.cs
+++ tactical-refactoring/extracting-classes/BlueBridge.SeaQuollMonitor.Management/ManagementModule.cs
@@ -10,6 +10,8 @@ namespace BlueBridge.SeaQuollMonitor.Management
             builder.RegisterType<FeaturesClientFactory>().As<IFeaturesClientFactory>().SingleInstance();
             builder.RegisterType<BaseMonitorRegistry>().As<IBaseMonitorRegistry>().SingleInstance();
             builder.RegisterType<LicenseAllocator>().As<ILicenseAllocator>().SingleInstance();
+            builder.RegisterType<LicenseAllocator2>().As<ILicenseAllocator2>().SingleInstance();
+            builder.RegisterType<ServerRepository>().As<IServerRepository>().SingleInstance();
         }
     }
 }
