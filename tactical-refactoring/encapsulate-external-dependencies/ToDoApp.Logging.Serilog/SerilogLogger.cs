5fb21b21a7939051fb3da44b6b3132f63b420adc
Aziz Sydikov <aziz.sydikov@red-gate.com>
2021-11-12 14:43:42 +0000

    Implement serilog logger

---
+++ tactical-refactoring/encapsulate-external-dependencies/ToDoApp.Logging.Serilog/SerilogLogger.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Serilog;
+
+namespace ToDoApp.Logging.Serilog
+{
+    public class SerilogLogger : ILogger
+    {
+        private global::Serilog.ILogger _logger;
+
+        public SerilogLogger(TextWriter textWriter)
+        {
+            _logger = new LoggerConfiguration().WriteTo.TextWriter(textWriter).CreateLogger();
+        }
+
+        public void Log(string message)
+        {
+            _logger.Information(message);
+        }
+    }
+}
